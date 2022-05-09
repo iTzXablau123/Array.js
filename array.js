edd8b292cd6609b022018ba9c65e4c3820c8f067
Xablau <102757880+iTzXablau123@users.noreply.github.com>
2022-05-09 15:34:43 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+//let notaHTML = 8;
+//let notaCSS = 7;
+//let notaJS = 10;
+
+//let media = (8+7+10)/3
+//console.log(media)
+
+let notas = [8, 7, 10] // <--- Array
+console.log(notas)
+
+console.log(notas[1]) // <--- posição
+console.log(notas.length) // <--- comprimento
---
+++ arraySybstituir.js
@@ -0,0 +1,3 @@
+let chamada = ['leo', 'Davi', 'Vk', 'Lai']
+chamada.splice(1, 2, 'Nicolly')
+
